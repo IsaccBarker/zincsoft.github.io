e5b6c52e4880aac9a30b4edad6a509e05132844e
BuildTools <unconfigured@null.spigotmc.org>
2022-06-11 18:25:32 -0600

    pretty

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        forest: "url('/assets/imgs/land.jpg')",
+        clouds: "url('/assets/imgs/clouds.png')"
+      }),
+      fontFamily: {
+        'display': ["Source Sans Pro"]
+      },
+      fontSize: {
+        'huge': '20rem'
+      }
+    },
   },
   plugins: [],
 }
