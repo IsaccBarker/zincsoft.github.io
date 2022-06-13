c890c9ece0532e44550c47304fdc82385d8ba3a6
BuildTools <unconfigured@null.spigotmc.org>
2022-06-12 21:38:00 -0600

    getting somewhere!

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,21 @@ module.exports = {
     extend: {
       backgroundImage: (theme) => ({
         forest: "url('/assets/imgs/land.jpg')",
-        clouds: "url('/assets/imgs/clouds.png')"
+        clouds: "url('/assets/imgs/clouds.png')",
+        farm: "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('/assets/imgs/farm.jpg')"
       }),
       fontFamily: {
         'display': ["Source Sans Pro"]
       },
       fontSize: {
         'huge': '20rem'
+      },
+      colors: {
+        'zinc-black',
+        'zinc-white',
+        'zinc-pearl',
+        'zinc-coral',
+        'zinc-blue',
       }
     },
   },
