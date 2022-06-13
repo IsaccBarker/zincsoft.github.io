4860b1036adc4f34719db02a1f2eb0d99491212d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-13 02:13:20 -0600

    some real progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,18 @@ module.exports = {
         farm: "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('/assets/imgs/farm.jpg')"
       }),
       fontFamily: {
-        'display': ["Source Sans Pro"]
+        'display': ["Source Sans Pro"],
+        'sub-mono': ["Source Code Pro"],
       },
       fontSize: {
         'huge': '20rem'
       },
       colors: {
-        'zinc-black',
-        'zinc-white',
-        'zinc-pearl',
-        'zinc-coral',
-        'zinc-blue',
+        'ex-black': '#000000',
+        'ex-white': '#F4F9E9',
+        'ex-pearl': '#CBC9AD',
+        'ex-coral': '#FE5E41',
+        'ex-blue':  '#0F7173',
       }
     },
   },
