ba760fcb5749d31d566d91b7664006ae271ab6fd
Isacc Barker <milobanks@zincsoft.dev>
2022-06-16 13:00:31 -0600

    setting working tree

--- src/projects/catnet/rfc/util/util.rb
+++ src/projects/catnet/rfc/util/util.rb
@@ -17,13 +17,22 @@ def pull
   Dir.chdir('../../../../../')
 
   system('git fetch catnet-rfcs main')
-  system('git subtree pull --prefix src/catnet/markdown catnet-rfcs main --squash')
+  system('git subtree pull --prefix src/projects/catnet/rfc/markdown catnet-rfcs main --squash')
 end
 
 def transpile(file)
-  template_data = File.open('template.html').read
+  template_data = File.open('rfc_x.html.template').read
   markdown_data = File.open(file).read
   html_data = Kramdown::Document.new(markdown_data).to_html
+
+  html_data = html_data.gsub("<p ", "<p class=\"text-xl lg:text-justify tracking-wide\" ")
+  html_data = html_data.gsub("<p>", "<p class=\"text-xl lg:text-justify tracking-wide\">")
+  html_data = html_data.gsub("<h1 ", "<h1 class=\"pt-8 font-display font-bold text-4xl md:text-6xl\" ")
+  html_data = html_data.gsub("<h2 ", "<h2 class=\"pt-8 font-display font-bold text-3xl md:text-5xl\" ")
+  html_data = html_data.gsub("<h3 ", "<h3 class=\"pt-8 font-display font-bold text-2xl md:text-4xl\" ")
+  html_data = html_data.gsub("<h4 ", "<h4 class=\"pt-8 font-display font-bold text-2xl md:text-4xl\" ")
+  html_data = html_data.gsub("<table>", "<table class=\"pt-16 text-xl lg:text-justify tracking-wide\">")
+  html_data = html_data.gsub("<ol>", "<ol class=\"text-xl lg:text-justify tracking-wide\">")
 
   template_data = template_data.gsub('<!-- TITLE -->', markdown_data.lines.first.chomp)
   template_data = template_data.gsub('<!-- HTML -->', html_data)
@@ -32,17 +41,16 @@ def transpile(file)
 end
 
 def get_new_index
-  index = File.open('../rfc.html.template').read
-  template_entry = '''<div style="padding-bottom: 25px">
-<a class="no-underline h5 bold text-accent" title="rfc-0-standardising-rfcs.html" href="<!-- HTML FILE -->">RFC <!-- RFC --></a>
-<h2 class="h1 lh-condensed col-9 mt-0"><a class="link-primary" title="rfc-0-standardising-rfcs.html" href="<!-- HTML FILE -->"><!-- TITLE --></a></h2>
-<a class="no-underline h5 bold text-accent" href="<!-- MARKDOWN FILE -->">download raw</a>
-</div>
-<br/>'''
+  index = File.open('index.html.template').read
+  template_entry = File.open('entry.html.template').read
   total = ""
 
   Dir.entries("../html").each do |file|
     if file == '.' or file == '..'
+      next
+    end
+
+    if not file.include? "html"
       next
     end
 
@@ -65,7 +73,7 @@ def get_new_index
 end
 
 if ARGV.length == 0
-  supply  
+  supply
 
   exit
 elsif ARGV[0] == '--help' or ARGV[0] == '-h'
@@ -96,8 +104,7 @@ elsif ARGV[0] == 'html'
   end
 
   puts 'Writting index...'
-  puts get_new_index
-  # File.open('../index.html', 'w') { |f| f.write get_new_index }
+  File.open('../index.html', 'w') { |f| f.write get_new_index }
 
   exit
 end
